47b6439e960743288a112fe06e8e4d9a55af92cb
Sven Anders Robbestad <robbestad@gmail.com>
2014-07-28 09:51:29 +0200

    first commit

---
+++ src/SysReports/SysReports.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace shopsys;
+
+class SysReports
+{
+
+    protected $report;
+
+    public function __construct()
+    {
+
+    }
+
+
+}
---
+++ tests/SysReportsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace shopsys\tests;
+
+use shopsys\SysReports;
+
+require __DIR__.'/../src/SysReports/SysReports.php';
+
+class SysReportsTest extends \PHPUnit_Framework_TestCase{
+
+
+    public function __construct(){
+    }
+
+    public function testInit(){
+    }
+
+
+}
